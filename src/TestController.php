409fdbf50f0c77271bf057050f19fb099ccce62a
Annie Kolemen <kolemen.annie@gmail.com>
2019-11-15 09:27:33 +0100

    Test Annie

--- src/TestController.php
+++ src/TestController.php
@@ -9,4 +9,23 @@ use EsnTest\Controller\EsnFetcher;
  */
 class TestController extends EsnFetcher {
 
+
+    /**
+     * Function returning an array with all the countries where ESN has a national level from the API.
+     */
+    public function countriesArray() {
+        if (!empty($resp[[
+            'data' => $this->getRequest("countries"),
+            'method' => 'GET',
+        ]])) {
+            return array ($resp);
+        }
+            var_dump($resp);
+    }
+
+    public function render() {
+
+    }
+
+
 }
